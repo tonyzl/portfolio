7f634cc0fb7a67166ce1170d12c0b07f127f311a
tonyzl <germanantoniosiete@gmail.com>
2023-11-15 16:05:13 -0500

    update

---
+++ components/main/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroContent from '../sub/HeroContent'
+
+
+
+export default function Hero() {
+  return (
+    <div className='relative flex flex-col h-full w-full'>
+
+        <HeroContent/>
+    </div>
+  )
+}
+
